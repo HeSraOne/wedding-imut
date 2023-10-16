748e49f78bd3e99cbd9df1af688042950adf6fea
HeSraOne <141596127+HeSraOne@users.noreply.github.com>
2023-10-16 13:46:25 +0700

    Update supabaseClient.ts

--- src/remotes/supabaseClient.ts
+++ src/remotes/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.https://agsxihjokaiijfoxbplt.supabase.co;
-const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnc3hpaGpva2FpaWpmb3hicGx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0MjAzMzksImV4cCI6MjAxMjk5NjMzOX0.qlr4eZCDy09cpwdIsCPDqzyW9WZznC3HCtK51h8Q0mM;
+const supabaseUrl = process.env.https://qaohxieqpcekpiyaxvlq.supabase.co;
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhb2h4aWVxcGNla3BpeWF4dmxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0Mzg0NjEsImV4cCI6MjAxMzAxNDQ2MX0.3-vC2_gU7qnBOOBJOvwKpkb5clLb4eRqIh63sTafKqk;
 
 if (supabaseUrl == null || supabaseAnonKey == null) {
   throw Error(
