d4f102b98a7620fd4c7a8dc40c2e16c29e8b058a
HeSraOne <141596127+HeSraOne@users.noreply.github.com>
2023-10-16 10:39:31 +0700

    Update supabaseClient.ts

--- src/remotes/supabaseClient.ts
+++ src/remotes/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.https://agsxihjokaiijfoxbplt.supabase.co;
+const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnc3hpaGpva2FpaWpmb3hicGx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0MjAzMzksImV4cCI6MjAxMjk5NjMzOX0.qlr4eZCDy09cpwdIsCPDqzyW9WZznC3HCtK51h8Q0mM;
 
 if (supabaseUrl == null || supabaseAnonKey == null) {
   throw Error(
