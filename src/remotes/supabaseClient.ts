e62f11dc445734e217117da5120bbb59407b81de
HeSraOne <141596127+HeSraOne@users.noreply.github.com>
2023-10-16 13:52:48 +0700

    Update supabaseClient.ts

--- src/remotes/supabaseClient.ts
+++ src/remotes/supabaseClient.ts
@@ -1,12 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.https://qaohxieqpcekpiyaxvlq.supabase.co;
-const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhb2h4aWVxcGNla3BpeWF4dmxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0Mzg0NjEsImV4cCI6MjAxMzAxNDQ2MX0.3-vC2_gU7qnBOOBJOvwKpkb5clLb4eRqIh63sTafKqk;
+import { createClient } from '@supabase/supabase-js'
 
-if (supabaseUrl == null || supabaseAnonKey == null) {
-  throw Error(
-    `[Supabase] Failed initialize client -> supabaseUrl: ${supabaseUrl} / supabaseAnonKey: ${supabaseAnonKey}`
-  );
-}
-
-export const api = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseUrl = 'https://qaohxieqpcekpiyaxvlq.supabase.co'
+const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhb2h4aWVxcGNla3BpeWF4dmxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0Mzg0NjEsImV4cCI6MjAxMzAxNDQ2MX0.3-vC2_gU7qnBOOBJOvwKpkb5clLb4eRqIh63sTafKqk
+const supabase = createClient(supabaseUrl, supabaseKey)
